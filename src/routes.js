3d279cda71cc8fe5f261990ecd5f7414e1d414fd
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-07-12 00:15:09 -0300

    static files, file uploads and model relation

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,21 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 // Only routes after this point will have this middleware applied
 routes.use(authMiddleware);
+routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
-
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
